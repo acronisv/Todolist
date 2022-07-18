35cc799e0c151f8a5d335a5ecacdae534f2ed51e
acronisv <acronisv@gmail.com>
2022-07-19 01:17:11 +0300

    Add component Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import './App.css';
-import Todolist from "./todolist";
+import {Todolist} from "./Todolist";
 
 function App() {
+    const task1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+    ]
+
+    const task2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false},
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title="What to learn" tasks={task1}/>
+            <Todolist title="Songs" tasks={task2}/>
         </div>
     );
 }
