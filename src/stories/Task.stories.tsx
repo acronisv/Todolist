9d570aec726292f022141ba12114e232c2d046bb
acronisv <acronisv@gmail.com>
2023-03-04 22:04:34 +0300

    Add storybook and snapshot tests

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {Task, TaskType} from "../TodolistWithRedux";
+import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
+import {useSelector} from "react-redux";
+import {AppRootState} from "../state/store";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Todolist/Task',
+    component: Task,
+    decorators: [ReduxStoreProviderDecorator]
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof Task>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const TaskCopy = () => {
+    const task = useSelector<AppRootState, TaskType>(state => state.tasks['todolistId1'][0])
+    return <Task task={task} id={'todolistId1'}/>
+}
+
+const Template: ComponentStory<typeof Task> = (args) => <TaskCopy/>;
+
+export const TaskStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
